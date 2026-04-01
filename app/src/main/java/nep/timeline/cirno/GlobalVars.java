f418f89ad7d2c25988fa5d481418666b448a6f2c
Timeline <53483352+Nep-Timeline@users.noreply.github.com>
2026-04-01 14:34:05 +0800

    Update CONFIG_DIR path from '/data/system/' to '/data/misc/'

--- app/src/main/java/nep/timeline/cirno/GlobalVars.java
+++ app/src/main/java/nep/timeline/cirno/GlobalVars.java
@@ -6,7 +6,7 @@ import nep.timeline.cirno.configs.settings.GlobalSettings;
 public class GlobalVars {
     public static final String TAG = "Cirno";
     public static final String CONFIG = "Cirno";
-    public final static String CONFIG_DIR = "/data/system/" + GlobalVars.CONFIG;
+    public final static String CONFIG_DIR = "/data/misc/" + GlobalVars.CONFIG;
     public final static String LOG_DIR = CONFIG_DIR + "/log";
     public static boolean isModuleActive = false;
     public static int XposedVersion = 0;
